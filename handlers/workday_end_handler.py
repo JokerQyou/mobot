1d72a77a48c3f4437802c95df020a129142f284a
Joker <joker@sd-79665.dedibox.fr>
2017-05-15 07:12:33 +0200

    Fix timezone issue

--- handlers/workday_end_handler.py
+++ handlers/workday_end_handler.py
@@ -1,11 +1,13 @@
 # coding: utf-8
 from datetime import datetime
+import logging
 import random
 
 import pytz
 
-from telegram.ext import BaseFilter, MessageHandler
+from telegram.ext import BaseFilter, Filters, MessageHandler
 
+log = logging.getLogger()
 TZ = pytz.timezone('Asia/Shanghai')
 START = 9
 END = 18
@@ -15,7 +17,7 @@ class WorkdayEndFilter(BaseFilter):
 
     def filter(self, message):
         # First filter by weekday: we don't work on weekends
-        date = message.date.replace(tzinfo=TZ)
+        date = TZ.normalize(message.date.replace(tzinfo=pytz.utc))
         if date.weekday() > 4:  # Weekdays are [0, 6]
             return False
 
@@ -24,7 +26,7 @@ class WorkdayEndFilter(BaseFilter):
             return False
 
         # Then filter by message text
-        text = unicode(message.text)
+        text = message.text
         keywords = (u'下班了', u'想下班', u'想回家', )
         time_query_keywords = (u'啥时', u'几点', u'什么时候', u'何時', )
         for keyword in keywords:
@@ -47,16 +49,16 @@ def workday_end_time(bot, update):
         '再敬业{}就能歇了',
     )
 
-    now = update.message.date.replace(tzinfo=TZ)
-    end_time = datetime(now.year, now.month, now.day, hour=18, tzinfo=TZ)
+    now = TZ.normalize(update.message.date.replace(tzinfo=pytz.utc))
+    end_time = TZ.localize(datetime(now.year, now.month, now.day, hour=18))
     duration = end_time - now
 
-    hour = duration.seconds / 3600
-    minute = (duration.seconds % 3600) / 60
-    time_remaining = '{} 小时 {} 分钟'.format(hour, minute)
+    hour = duration.seconds // 3600
+    minute = (duration.seconds % 3600) // 60
+    time_remaining = ' {} 小时 {} 分钟'.format(hour, minute)
 
     text = random.choice(text_templates).format(time_remaining)
     bot.send_message(chat_id=update.message.chat_id, text=text)
 
 
-workday_end_handler = MessageHandler(WorkdayEndFilter(), workday_end_time)
+workday_end_handler = MessageHandler(Filters.text & WorkdayEndFilter(), workday_end_time)
