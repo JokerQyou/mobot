545ae45de06df37e4ab1f720692a6e1259e06c0b
joker <joker@ci>
2017-06-17 15:06:00 +0200

    Fix importing

--- mobot/bot.py
+++ mobot/bot.py
@@ -4,7 +4,6 @@ import multiprocessing
 # from multiprocessing.pool import Pool
 
 from telegram.ext import Updater
-from .taskqueue import tiger
 from .handlers import HANDLERS
 from . import CONFIG
 
@@ -20,11 +19,11 @@ def start_taskqueue_workers():
     '''
     # 先添加任务，因为这时候 worker 没有在运行，所以 unique 任务不会出现重复
     from .handlers import PERIODIC_TASKS
+    from .taskqueue import tiger
     for func, period_ in PERIODIC_TASKS:
         tiger.task(schedule=period_, unique=True)(func).delay()
 
     # 创建并运行 worker
-    from .taskqueue import tiger
     p = multiprocessing.Process(target=tiger.run_worker)
     p.daemon = True
     p.start()
